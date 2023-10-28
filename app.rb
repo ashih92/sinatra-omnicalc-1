7af7cce3ffa62ef1ce9a9ae7006462a59956f5ef
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-10-28 18:08:44 +0000

    committ

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:end)
+end 
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get '/sqaure/results' do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
