b821ca0163e786e9e743614677cc8dc140b780ac
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-10-28 23:17:03 +0000

    commit

--- app.rb
+++ app.rb
@@ -39,11 +39,26 @@ get '/payment/results' do
   @the_r = @the_APR/12
   @the_years = params.fetch("users_years").to_f
   @the_periods = @the_years * 12
-  @the_principal = params.fetch("users_principal").to_currency
+  @the_principal = params.fetch("users_principal").to_f
+  #@the_principal = sprintf("$%.2f", @the_principal)
   @numerator = @the_principal * @the_r
   @denominator = 1 - (1+@the_r)**(-1*@the_periods)
   @the_result = @numerator/@denominator
+  @the_result = sprintf("$%.2f", @the_result)
+
+  @the_APR = sprintf("%.2f%%",@the_APR*100)
   erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_calc)
+end
+
+get '/random/results' do
+  @the_minimum = params.fetch("users_minimum").to_f
+  @the_maximum = params.fetch("users_maximum").to_f
+  @the_result = rand(@the_minimum..@the_maximum)
+  erb(:random_results)
 end
 
 get("/") do
